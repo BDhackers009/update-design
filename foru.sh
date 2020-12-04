d8d2c7a78ca9a0fd6f3e7fc873ceb6c567904e4a
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2020-12-04 23:24:07 +0600

    Update foru.sh

--- foru.sh
+++ foru.sh
@@ -1,41 +1,20 @@
 #!/usr/bin/bash
 
-r='\[\e[1;31m\]'
-g='\[\e[1;32m\]'
-y='\[\e[1;33m\]'
-b='\[\e[1;34m\]'
-p='\[\e[1;35m\]' 
-c='\[\e[1;36m\]' 
-w='\[\e[1;37m\]'
 echo ""
-echo ""
-termux-setup-storage
+read -p $' \e[0m\e[1;92m Input your Username : \e[0m\e[1;96m\en' user
 echo ""
 echo ""
 echo -e  $g "Please  Wait  A few Minutes...... "
 echo ""
 apt update 
 apt upgrade -y 
-
 pkg install figlet -y
-
 pkg install ncurses-utils -y 
-
 pkg install ruby -y 
-apt install ruby -y
 gem install lolcat 
-
 figlet -f big Wait | lolcat
-
-
 git clone https://github.com/BDhackers009/simple-edit
-cd simple-edit
-cp bash.bashrc  /data/data/com.termux/files/usr/etc
-cp wlc.py  /data/data/com.termux/files/usr/etc
-cd
+sed 's+THBD+'$user'+g' simple-edit/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+THBD+'$user'+g' simple-edit/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
 rm -rf simple-edit
 figlet -f big BDh@Ckers | lolcat
-
-bash
-
-exit
